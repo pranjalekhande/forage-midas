6b262f474c4ce99de9184ca376f98e76f01b98e2
Pranjal Ekhande <pranjalekhande11@gmail.com>
2025-01-27 19:11:42 -0500

    TaskTwo
    Added Transaction Listener in Components and added Debug statement in the application properties

---
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -0,0 +1,20 @@
+package com.jpmc.midascore.component;
+
+
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TransactionListener {
+	@KafkaListener(topics = "${general.kafka-topic}", groupId = "${spring.kafka.consumer.group-id}")
+	
+	public void listen(Transaction transaction) {
+        System.out.println("Received Transaction: " + transaction);
+        System.out.println("Sender ID: " + transaction.getSenderId());
+        System.out.println("Recipient ID: " + transaction.getRecipientId());
+        System.out.println("Amount: " + transaction.getAmount());
+    }
+
+}
